c1c16af94a2b1148c6ead1857af9c3212c649754
Umut Toker <utoker@gmail.com>
2021-09-15 19:52:29 -0400

    add comment post and get requests

--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -1,25 +1,59 @@
-import { useState } from 'react';
-
-import classes from './Comments.module.css';
-import NewCommentForm from './NewCommentForm';
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import useHttp from "../../hooks/use-http";
+import { getAllComments } from "../../lib/api";
+import LoadingSpinner from "../UI/LoadingSpinner";
+import classes from "./Comments.module.css";
+import NewCommentForm from "./NewCommentForm";
+import CommentsList from "./CommentsList";
+import { useCallback } from "react";
 
 const Comments = () => {
+  const params = useParams();
+  const { quoteId } = params;
   const [isAddingComment, setIsAddingComment] = useState(false);
-
   const startAddCommentHandler = () => {
     setIsAddingComment(true);
   };
-  
+
+  const { sendRequest, status, data: loadedComments } = useHttp(getAllComments);
+  const addedCommentHandler = useCallback(() => {
+    sendRequest({ quoteId });
+  }, [sendRequest, quoteId]);
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [quoteId, sendRequest]);
+
+  let comments;
+
+  if (status === "pending") {
+    comments = (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+  if (status === "completed" && loadedComments && loadedComments.length) {
+    comments = <CommentsList comments={loadedComments} />;
+  }
+  if (
+    status === "completed" &&
+    (!loadedComments || loadedComments.length === 0)
+  ) {
+    comments = <p className="centered">No comments were added yet!</p>;
+  }
   return (
     <section className={classes.comments}>
       <h2>User Comments</h2>
       {!isAddingComment && (
-        <button className='btn' onClick={startAddCommentHandler}>
+        <button className="btn" onClick={startAddCommentHandler}>
           Add a Comment
         </button>
       )}
-      {isAddingComment && <NewCommentForm />}
-      <p>Comments...</p>
+      {isAddingComment && (
+        <NewCommentForm quoteId={quoteId} onAddComment={addedCommentHandler} />
+      )}
+      {comments}
     </section>
   );
 };
--- src/components/comments/NewCommentForm.js
+++ src/components/comments/NewCommentForm.js
@@ -1,26 +1,42 @@
-import { useRef } from 'react';
+import { useRef } from "react";
+import { useEffect } from "react/cjs/react.development";
+import useHttp from "../../hooks/use-http";
+import { addComment } from "../../lib/api";
+import LoadingSpinner from "../UI/LoadingSpinner";
 
-import classes from './NewCommentForm.module.css';
+import classes from "./NewCommentForm.module.css";
 
-const NewCommentForm = (props) => {
+const NewCommentForm = ({ onAddComment, quoteId }) => {
   const commentTextRef = useRef();
+  const { sendRequest, status, data, error } = useHttp(addComment);
+
+  useEffect(() => {
+    if (status === "completed" && !error) {
+      onAddComment();
+    }
+  }, [status, error, onAddComment]);
 
   const submitFormHandler = (event) => {
     event.preventDefault();
-
     // optional: Could validate here
 
-    // send comment to server
+    const enteredText = commentTextRef.current.value;
+    sendRequest({ commentdata: { text: enteredText }, quoteId });
   };
 
   return (
     <form className={classes.form} onSubmit={submitFormHandler}>
+      {status === "pending" && (
+        <div className="centered">
+          <LoadingSpinner />
+        </div>
+      )}
       <div className={classes.control} onSubmit={submitFormHandler}>
-        <label htmlFor='comment'>Your Comment</label>
-        <textarea id='comment' rows='5' ref={commentTextRef}></textarea>
+        <label htmlFor="comment">Your Comment</label>
+        <textarea id="comment" rows="5" ref={commentTextRef}></textarea>
       </div>
       <div className={classes.actions}>
-        <button className='btn'>Add Comment</button>
+        <button className="btn">Add Comment</button>
       </div>
     </form>
   );
